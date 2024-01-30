c4d01ba64b846add75564c33698affd7f5f94adf
Muasa-harman <harman.muasa@gmail.com>
2024-01-30 10:31:24 +0300

    create get post api route

--- backend/routes/post.js
+++ backend/routes/post.js
@@ -1,9 +1,10 @@
 import  express  from "express";
 import {verifyToken} from '../utils/verifyUser.js'
-import { createPost } from "../controllers/postController.js";
+import { createPost, getposts } from "../controllers/postController.js";
 
 const router = express.Router();
 
 router.post("/create", verifyToken,createPost)
+router.get("/getposts", getposts)
 
 export default router;
