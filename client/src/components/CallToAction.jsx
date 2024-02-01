f05f2fe089a6dfd277d6dd9be1cd6255ccfe2526
Muasa-harman <harman.muasa@gmail.com>
2024-02-01 17:54:42 +0300

    full-stack javascript application deployment

--- client/src/components/CallToAction.jsx
+++ client/src/components/CallToAction.jsx
@@ -4,8 +4,8 @@ const CallToAction = () => {
   return (
     <div className="flex flex-col sm:flex-row p-3 border border-teal-500 justify-center items-center rounded-tl-3xl rounded-br-3xl text-center">
       <div className="flex-1 justify-center flex flex-col">
-        <h2 className="text-2xl">Want to learn more about Javascript?</h2>
-        <p className="text-gray-500 my-2">Checkout these resources with 100 Jaascript Projects</p>
+        <h2 className="text-2xl">Want to learn more about Javascript/Typescript?</h2>
+        <p className="text-gray-500 my-2">Checkout these resources with 100 Jaascript & Typescript Projects</p>
         <button className="rounded-bl-none bg-gradient-to-r focus:outline from-gray-500 via-slate-500 to-gray-800 rounded-lg text-white p-2"> <a  href="https://myportfolioblogapp.onrender.com/" target="_blank" rel="noopener noreferrer">100 JavaScript Projects</a></button>
       </div>
       <div className="p-7 flex-1">
--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,11 +1,34 @@
-import React from 'react'
-import Modal from '../components/Modal'
+import React from "react";
 
 export const About = () => {
   return (
-    <div className=''>
-      About
-      <Modal/>
+    <div className="min-h-screen flex items-center justify-center ">
+      <div className="max-w-2xl mx-auto p-3 text-center">
+        <div className="">
+          <h1 className="text-3xl font font-semibold text-center my-7">About Harman Muasa Blog</h1>
+          <div className="text-md text-gray-500 flex flex-col gap-6">
+            <p className="">
+              Harman Blog is a blog I have created to showcase my skills,share
+              my thoughts and idesa with the world I am a full stack software
+              engineer an i love to write about my skills and knowledge and
+              thing that i have learned as adeveloper. I hope you enjoy my piece
+              of work.
+            </p>
+            <p className="">
+              In this blog, you will find oftenly articles on latest trw=ends on
+              topics such as web development, software engineering and
+              programming languages. Harman is always learning new technologies
+              so be sure to Check oftenly for more pieces.
+            </p>
+            <p className="">
+              We encourage you to leave comments on our posts and engage with
+              other readers, You can like other people comments and reply to
+              them as well. We believe that a community of leaners can help each
+              other grow and improve technicallly
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
