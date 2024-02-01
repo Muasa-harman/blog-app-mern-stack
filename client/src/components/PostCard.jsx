7ab37f128e48edf36125787dba8f14420329ef1c
Muasa-harman <harman.muasa@gmail.com>
2024-02-01 09:19:53 +0300

    add recent article component on post page

---
+++ client/src/components/PostCard.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const PostCard = ({ post }) => {
+  return (
+    <div className="group relative w-full border border-teal-400 hover:border-2 h-[300px] overflow-hidden rounded-lg sm:w-[430px] transition-all">
+      <Link to={`/post/${post.slug}`}>
+        <img
+          src={post.image}
+          alt="post cover"
+          className="h-[60px] w-full object-cover group-hover:h-[100px] transition-all duration-300 -z-20"
+        />
+      </Link>
+      <div className="p-3 flex flex-col gap-2">
+        <p className="text-lg font-semibold line-clamp-2">{post.title}</p>
+        <span className="italic text-sm">{post.category}</span>
+        <Link to={`/post/${post.slug}`}
+         className=" group-hover:bottom-0 absolute bottom-[-200px] left-0 right-0 border border-teal-400 text-teal-400 hover:bg-teal-400 hover:text-white transition-all duration-300 text-center py-2 rounded-md !rounded-tl-none m-2">
+            Read article
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default PostCard;
--- client/src/pages/PostPage.jsx
+++ client/src/pages/PostPage.jsx
@@ -4,12 +4,14 @@ import { SpinnerCircular } from "spinners-react";
 import { Link } from "react-router-dom";
 import CallToAction from "../components/CallToAction";
 import CommentSection from "../components/CommentSection";
+import PostCard from "../components/PostCard";
 
 const PostPage = () => {
   const { postSlug } = useParams();
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
   const [post, setPost] = useState(null);
+  const [recentPosts,setRecentPosts] = useState(null);
 
   useEffect(() => {
     const fetchPost = async () => {
@@ -34,6 +36,21 @@ const PostPage = () => {
     };
     fetchPost();
   }, [postSlug]);
+
+  useEffect(()=>{
+    try {
+      const fetchRecentPosts = async() =>{
+        const res = await fetch(`/api/post/getposts?limit=3`);
+        const data = await res.json();
+        if(res.ok){
+          setRecentPosts(data.posts);
+        }
+      };
+      fetchRecentPosts();
+    } catch (error) {
+      console.log(error.message);
+    }
+  },[]);
 
   if (loading) return;
   <div className="flex justify-cente items-center min-h-screen">
@@ -68,6 +85,15 @@ const PostPage = () => {
             <CallToAction/>
         </div>
         <CommentSection postId={post._id}/>
+
+        <div className="flex flex-col justify-center items-center mb-5">
+          <h1 className="text-xl mt-5">Recent articles</h1>
+          <div className="flex flex-wrap gap-3 mt-5 justify-center">
+            {recentPosts && recentPosts.map((post)=>(
+              <PostCard key={post._id} post={post}/>
+            ))}
+          </div>
+        </div>
     </main>
   );
 };
