f6a493739d66399e4faff00382aba4e3234e29d7
Muasa-harman <harman.muasa@gmail.com>
2024-03-02 22:47:00 +0300

    post image update

--- client/src/pages/ProjectsPage.jsx
+++ client/src/pages/ProjectsPage.jsx
@@ -6,19 +6,19 @@ import Finance from "../assets/dashboard.jpg"
 import Property from "../assets/Listing.jpg"
 import Delivery from "../assets/landing.png"
 
-const ProjectsPage = ({image,title,category}) => {
+const ProjectsPage = () => {
   return (
     <div>
       <div className="max-w-6xl mx-auto p-3 flex flex-col gap-8 py-7">
           <div className="flex flex-col gap-6">
             <h2 className="text-2xl font-semibold text-center">Recent Projects</h2>
             <div className="flex flex-wrap gap-4 justify-center">
-                <ProjectCard title={"Hotel Reservation System"} category={"Full-Stack application"} image={Hotel} link={"https://hotel-reservation-app-uesu.onrender.com/"}/>
-                <ProjectCard title={"Financial Graphs Dashboard"} category={"React and Django"} image={Finance} link={"https://yala-finance-dashboard-demo.vercel.app/"}/>
-                <ProjectCard title={"Property management System"} category={"Full-Stack application"} image={Property} link={"https://property-management-rh6h.onrender.com/"}/>
-                <ProjectCard title={"Food Delivery Application"} category={"Full-Stack application"} image={Delivery} link={"https://donfiles-online-food-delivery-app.onrender.com"}/>
-                <ProjectCard title={"Data visualisation Saas Application"} category={"Full-Stack application"} image={Delivery}/>
-                <ProjectCard title={"Work in Progress"} category={"Django"} image={Property}/>
+                <ProjectCard title={"Hotel Reservation System"}  image={Hotel} link={"https://hotel-reservation-app-uesu.onrender.com/"}/>
+                <ProjectCard title={"Financial Graphs Dashboard"} image={Finance} link={"https://yala-finance-dashboard-demo.vercel.app/"}/>
+                <ProjectCard title={"Property management System"}  image={Property} link={"https://property-management-rh6h.onrender.com/"}/>
+                <ProjectCard title={"Food Delivery Application"}  image={Delivery} link={"https://donfiles-online-food-delivery-app.onrender.com"}/>
+                <ProjectCard title={"Data visualisation Saas Application"}  image={Delivery}/>
+                <ProjectCard title={"Work in Progress"}  image={Property}/>
             </div>
             {/* <Link to={'/search'} className="text-lg text-teal-500 hover:underline text-center">View all posts</Link> */}
             </div>
