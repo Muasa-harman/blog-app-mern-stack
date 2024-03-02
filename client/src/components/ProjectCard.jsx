3b93cd212547ceabad1d11dbefa4054b54aaa580
Muasa-harman <harman.muasa@gmail.com>
2024-03-02 22:37:21 +0300

    links update

--- client/src/components/ProjectCard.jsx
+++ client/src/components/ProjectCard.jsx
@@ -1,20 +1,20 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-const ProjectCard = ({image,title,category}) => {
+const ProjectCard = ({image,title,category,link}) => {
   return (
     <div className="group relative w-full border border-teal-400 hover:border-2 h-[300px] overflow-hidden rounded-lg sm:w-[430px] transition-all">
       <Link to={``}>
         <img
-          src={""}
+          src={image}
           alt="post cover"
-          className="h-[60px] w-full object-cover group-hover:h-[100px] transition-all duration-300 -z-20"
+          className="h-[300px] w-full object-cover group-hover:h-[200px] transition-all duration-300 -z-20"
         />
       </Link>
       <div className="p-3 flex flex-col gap-2">
         <p className="text-lg font-semibold line-clamp-2">{title}</p>
         <span className="italic text-sm">{category}</span>
-        <Link to={``}
+        <Link to={link}
          className=" group-hover:bottom-0 absolute bottom-[-200px] left-0 right-0 border border-teal-400 text-teal-400 hover:bg-teal-400 hover:text-white transition-all duration-300 text-center py-2 rounded-md !rounded-tl-none m-2">
             View Project
         </Link>
--- client/src/pages/ProjectsPage.jsx
+++ client/src/pages/ProjectsPage.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import ProjectCard from '../components/ProjectCard'
+import Hotel from "../assets/kigali.jpg"
+import Finance from "../assets/dashboard.jpg"
+import Property from "../assets/Listing.jpg"
+import Delivery from "../assets/landing.png"
 
 const ProjectsPage = ({image,title,category}) => {
   return (
@@ -9,12 +13,12 @@ const ProjectsPage = ({image,title,category}) => {
           <div className="flex flex-col gap-6">
             <h2 className="text-2xl font-semibold text-center">Recent Projects</h2>
             <div className="flex flex-wrap gap-4 justify-center">
-                <ProjectCard title={"Hotel Reservation System"} category={"Full-Stack application"}/>
-                <ProjectCard title={"Financial Graphs Dashboard"} category={"React and Django"}/>
-                <ProjectCard title={"Property management System"} category={"Full-Stack application"}/>
-                <ProjectCard title={"Food Delivery Application"} category={"Full-Stack application"}/>
-                <ProjectCard title={"Data visualisation Saas Application"} category={"Full-Stack application"}/>
-                <ProjectCard title={"Work in Progress"} category={"Django"}/>
+                <ProjectCard title={"Hotel Reservation System"} category={"Full-Stack application"} image={Hotel} link={"https://hotel-reservation-app-uesu.onrender.com/"}/>
+                <ProjectCard title={"Financial Graphs Dashboard"} category={"React and Django"} image={Finance} link={"https://yala-finance-dashboard-demo.vercel.app/"}/>
+                <ProjectCard title={"Property management System"} category={"Full-Stack application"} image={Property} link={"https://property-management-rh6h.onrender.com/"}/>
+                <ProjectCard title={"Food Delivery Application"} category={"Full-Stack application"} image={Delivery} link={"https://donfiles-online-food-delivery-app.onrender.com"}/>
+                <ProjectCard title={"Data visualisation Saas Application"} category={"Full-Stack application"} image={Delivery}/>
+                <ProjectCard title={"Work in Progress"} category={"Django"} image={Property}/>
             </div>
             {/* <Link to={'/search'} className="text-lg text-teal-500 hover:underline text-center">View all posts</Link> */}
             </div>
