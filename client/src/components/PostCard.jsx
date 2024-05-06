411a82e0bba9e3d3a1f58d31a2178c50e3ca7592
Muasa-harman <harman.muasa@gmail.com>
2024-05-06 10:12:49 +0300

    post card update

--- client/src/components/PostCard.jsx
+++ client/src/components/PostCard.jsx
@@ -13,7 +13,7 @@ const PostCard = ({ post }) => {
       </Link>
       <div className="p-3 flex flex-col gap-2">
         <p className="text-lg font-semibold line-clamp-2">{post.title}</p>
-        <span className="italic text-sm">{"Data Structures and Algorithms"}</span>
+        <span className="italic text-sm">{"Data Structures and Algorithms <br/> Python"}</span>
         {/* post.category */}
         <Link to={`/post/${post.slug}`}
          className=" group-hover:bottom-0 absolute bottom-[-200px] left-0 right-0 border border-teal-400 text-teal-400 hover:bg-teal-400 hover:text-white transition-all duration-300 text-center py-2 rounded-md !rounded-tl-none m-2">
