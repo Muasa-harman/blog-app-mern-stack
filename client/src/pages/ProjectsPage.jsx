ee5e8e087eb4acadad6a6c200796ba4239008230
Muasa-harman <harman.muasa@gmail.com>
2024-09-13 20:19:11 +0300

    projects page

--- client/src/pages/ProjectsPage.jsx
+++ client/src/pages/ProjectsPage.jsx
@@ -17,15 +17,15 @@ const ProjectsPage = () => {
           <div className="flex flex-col gap-6">
             <h2 className="text-2xl font-semibold text-center">Recent Projects</h2>
             <div className="flex flex-wrap gap-4 justify-center">
-                <ProjectCard title={"Hotel Reservation System"}  image={Hotel} link={"https://hotel-reservation-app-uesu.onrender.com/"}/>
+                <ProjectCard title={"ECommerce"}  image={Harman} link={"https://online-shopping-app-v46x.vercel.app/"}/>
+                <ProjectCard title={"Data Dashboard"}  image={Dashboard} link={"https://savanna-test-beta.vercel.app/"}/>
                 <ProjectCard title={"Financial Graphs Dashboard"} image={Finance} link={"https://yala-finance-dashboard-demo.vercel.app/"}/>
+                <ProjectCard title={"E-commerce shop application"}  image={Ecommerce} link={'https://next-js-ecommerce-bspt103t6-yala-finance-dashboards-projects.vercel.app/'}/>
+                <ProjectCard title={"Hotel Reservation System"}  image={Hotel} link={"https://hotel-reservation-app-uesu.onrender.com/"}/>
                 <ProjectCard title={"Property management System"}  image={Property} link={"https://property-management-rh6h.onrender.com/"}/>
                 <ProjectCard title={"Food Delivery Application"}  image={Delivery} link={"https://donfiles-online-food-delivery-app.onrender.com"}/>
-                <ProjectCard title={"E-commerce shop application"}  image={Ecommerce} link={'https://next-js-ecommerce-bspt103t6-yala-finance-dashboards-projects.vercel.app/'}/>
                 <ProjectCard title={"Work in Progress"}  image={Property}/>
-                <ProjectCard title={"Data Dashboard"}  image={Dashboard} link={"https://savanna-test-beta.vercel.app/"}/>
                 <ProjectCard title={"POS System"}  image={POS} link={"https://pointofsalesystem-pos.onrender.com/"}/>
-                <ProjectCard title={"ECommerce"}  image={Harman} link={"https://online-shopping-app-v46x.vercel.app/"}/>
             </div>
             {/* <Link to={'/search'} className="text-lg text-teal-500 hover:underline text-center">View all posts</Link> */}
             </div>
