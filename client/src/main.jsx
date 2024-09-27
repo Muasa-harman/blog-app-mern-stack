1d4aa88690f1b210b61957ff70a49571eef6da6e
Muasa-harman <harman.muasa@gmail.com>
2024-09-27 17:23:38 +0300

    render error

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,7 +1,7 @@
+import './index.css'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
 import {store,persistor} from "./redux/store.js";
 import {Provider} from "react-redux"
 import { PersistGate } from 'redux-persist/lib/integration/react';
