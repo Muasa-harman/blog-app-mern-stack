bc9955c9cb4d5d4811fbfd9ecc6c1ff6718168d1
Muasa-harman <harman.muasa@gmail.com>
2024-10-03 19:23:54 +0300

    blog link

--- backend/server.js
+++ backend/server.js
@@ -41,6 +41,6 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
 });
 
-app.listen(5000, () => {
-  console.log("Server is running on port 3000");
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
